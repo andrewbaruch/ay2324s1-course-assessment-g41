69cafcc5c30eb2169ef462cf91ebbe73401a81e4
Didymus <didymusne@gmail.com>
2023-09-23 23:34:53 +0800

    [frontend] feat: add peerprep logo

--- frontend/src/components/header/index.tsx
+++ frontend/src/components/header/index.tsx
@@ -1,16 +1,30 @@
 import { useHeaderTab } from "@/hooks/useHeaderTabs";
-import { Box, Button, Container, HStack } from "@chakra-ui/react";
+import { Box, Button, Container, HStack, Image } from "@chakra-ui/react";
 
 export const Header = () => {
   const { goToBrowsePage } = useHeaderTab();
 
   return (
-    <Box w="100%" boxShadow="sm" bgColor={"gray.500"} h="10vh">
-      <Container maxW="8xl" p={4}>
-        <HStack w="full">
-          <Button onClick={goToBrowsePage}>Browse</Button>
-        </HStack>
-      </Container>
-    </Box>
+    <Container
+      w="100%"
+      bgColor={"white"}
+      h="10vh"
+      borderWidth={1}
+      maxW="8xl"
+      p={2}
+    >
+      <HStack w="full" h="100%">
+        <Box
+          w={180}
+          alignSelf={"center"}
+          _hover={{ cursor: "pointer" }}
+          onClick={goToBrowsePage}
+        >
+          <Image src={"./logo.svg"} objectFit={"cover"} />
+        </Box>
+
+        <Button onClick={goToBrowsePage}>Browse</Button>
+      </HStack>
+    </Container>
   );
 };
