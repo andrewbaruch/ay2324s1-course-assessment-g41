8b23a74121bbcbb507448c9099c80778197e47f5
Didymus <didymusne@gmail.com>
2023-10-08 23:10:28 +0800

    [frontend] feat: add username cursor for collab editor

--- frontend/src/views/codeEditor/CodeEditor.tsx
+++ frontend/src/views/codeEditor/CodeEditor.tsx
@@ -1,40 +1,91 @@
-import { useRef } from "react"
+import { useEffect, useState } from "react"
 import { editor } from 'monaco-editor'
 import * as Y from "yjs"
-import { WebsocketProvider } from 'y-websocket';
 import { MonacoBinding } from 'y-monaco';
 import { Editor } from "@monaco-editor/react";
-import { SocketIOProvider } from "y-socket.io";
+import { HocuspocusProvider } from "@hocuspocus/provider";
+import { Cursor } from "../../components/editor";
 
+// TODO: @didy move to environment
 const serverWsUrl = "ws://localhost:1234"
 
-export const CodeEditor = () => {
-  const editorRef = useRef<editor.IStandaloneCodeEditor>();
+// TODO: @didy refactor
+const options: editor.IStandaloneEditorConstructionOptions = {
+  autoIndent: 'full',
+  contextmenu: true,
+  fontFamily: 'monospace',
+  fontSize: 13,
+  lineHeight: 24,
+  hideCursorInOverviewRuler: true,
+  matchBrackets: 'always',
+  minimap: {
+    enabled: false,
+  },
+  scrollbar: {
+    horizontalSliderSize: 4,
+    verticalSliderSize: 4,
+  },
+  selectOnLineNumbers: false,
+  roundedSelection: false,
+  readOnly: false,
+  cursorStyle: 'line',
+  automaticLayout: true,
+};
 
-  const handleEditorMount = (editor: editor.IStandaloneCodeEditor) => {
-    editorRef.current = editor
+/**
+ * Reference: https://liveblocks.io/examples/collaborative-code-editor/nextjs-yjs-monaco
+ */
+export const CodeEditor = () => {
+  const [editorRef, setEditorRef] = useState<editor.IStandaloneCodeEditor>()
+  const [provider, setProvider] = useState<HocuspocusProvider>();
+
+  useEffect(() => {
+    if (!editorRef) {
+      return
+    }
+
+    let yProvider: HocuspocusProvider;
+    let yDoc: Y.Doc;
+    let binding: MonacoBinding;
 
     // Initialise yjs
-    const doc = new Y.Doc()
+    yDoc = new Y.Doc()
 
     // Connect to peers with WebSocket
-    const provider: WebsocketProvider = new WebsocketProvider(serverWsUrl, "test", doc)
-    const type = doc.getText('monaco')
+    yProvider = new HocuspocusProvider({
+      url: serverWsUrl,
+      name: "test", // room name
+      document: yDoc,
+    })
+    setProvider(yProvider)
 
     // Bind yjs doc to monaco editor
-    const model = editor.getModel()
-    if (model) {
-      const binding = new MonacoBinding(type, model, new Set([editorRef.current]), provider.awareness)
-      provider.connect()
-      console.log(provider)
+    const yText = yDoc.getText('monaco')
+    const model = editorRef.getModel()
+    binding = new MonacoBinding(yText, model as editor.ITextModel, new Set([editorRef]), yProvider.awareness)
+  }, [editorRef])
+
+  useEffect(() => {
+    if (!provider) {
+      return
     }
+
+  }, [provider])
+
+  const handleEditorMount = (editor: editor.IStandaloneCodeEditor) => {
+    setEditorRef(editor)
   }
 
   return (
-    <Editor
-      height="60vh"
-      defaultValue={'// your code here'}
-      onMount={handleEditorMount}
-    />
+    <>
+      {provider ? < Cursor yProvider={provider} /> : null}
+      <Editor
+        height="60vh"
+        defaultValue={'// your code here'}
+        onMount={handleEditorMount}
+        language={'python'}
+        options={options}
+      />
+    </>
   )
 }
