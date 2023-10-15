7f55b8ad07d47cc927619f1b2d1695dcf65f2994
Toh Kar Wi <karwitoh@bytedance.com>
2023-10-15 16:41:03 +0800

    feat: config gateway

--- frontend/src/utils/api.ts
+++ frontend/src/utils/api.ts
@@ -1,18 +1,18 @@
 // BE_API contains api endpoints we use to query our host backend
 export const BE_API = {
   auth: {
-    checkAuth: "/auth/checkAuth",
-    google: "/auth/google",
-    refresh: "/auth/refresh",
-    logout: "/auth/logout",
+    checkAuth: "/user/auth/checkAuth",
+    google: "/user/auth/google",
+    refresh: "/user/auth/refresh",
+    logout: "/user/auth/logout",
   },
   users: {
-    root: "/user",
-    topics: "/user/topics",
+    root: "/user/user",
+    topics: "/user/user/topics",
     user: (id: string) => `/user/${id}`,
     // karwi: remove this
-    uploadImageUrl: "/user/uploadImageUrl",
+    uploadImageUrl: "/user/user/uploadImageUrl",
   },
-  topics: "/topics",
-  languages: "/languages",
+  topics: "/user/topics",
+  languages: "/user/languages",
 };
