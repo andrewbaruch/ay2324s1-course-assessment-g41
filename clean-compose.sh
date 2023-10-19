20728cac5c838636017da943e4eb739e4f4efeb6
hpkoh <hpkoh99@gmail.com>
2023-10-19 23:48:01 +0800

    Revert "feat: restore dev set up for docker"
    This reverts commit 0f5864c0a5e406b170da7a118424ac4203034691.

--- clean-compose.sh
+++ clean-compose.sh
@@ -3,7 +3,7 @@ docker-compose rm -f
 docker volume rm ay2324s1-course-assessment-g41_3219User
 docker volume rm ay2324s1-course-assessment-g41_mongo
 docker volume rm ay2324s1-course-assessment-g41_postgres
-# docker volume rm ay2324s1-course-assessment-g41_frontend
+docker volume rm ay2324s1-course-assessment-g41_frontend
 
 docker-compose build --no-cache
 docker-compose up
