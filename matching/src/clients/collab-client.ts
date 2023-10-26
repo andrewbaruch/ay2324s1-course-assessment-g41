790e935bda6fb054de07cef55adb56d9f5b1d49a
Didymus <didymusne@gmail.com>
2023-10-26 14:56:18 +0800

    [collab+matching] fix: update nginx endpoint for docker env

--- matching/src/clients/collab-client.ts
+++ matching/src/clients/collab-client.ts
@@ -4,15 +4,22 @@ const { COLLAB_SERVICE_ENDPOINT } = process.env
 
 const collabClient = axios.create({
   baseURL: COLLAB_SERVICE_ENDPOINT,
+  headers: {
+    'Content-Type': 'application/json',
+    'Accept': "*/*",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Connection": "keep-alive",
+  }
 });
 
 const createRoom = async (userId1: string, userId2: string) => {
+  await collabClient.put(`/room/${1}`)
   return await collabClient.post("/room", {
     userId1, userId2
   })
 }
 
-const closeRoom = async (roomId: string) => {
+const closeRoom = async (roomId: number) => {
   await collabClient.put(`/${roomId}`)
 }
 
