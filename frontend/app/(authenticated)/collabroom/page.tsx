0d1f6de47523c09ab78e447be2b20b1f2f6c7408
Toh Kar Wi <karwitoh@bytedance.com>
2023-10-26 21:32:13 +0800

    chore: clean up redundant useAuthenticated

--- frontend/app/(authenticated)/collabroom/page.tsx
+++ frontend/app/(authenticated)/collabroom/page.tsx
@@ -1,10 +1,7 @@
 "use client";
 
 import { Room } from "@/views/collabroom";
-import useAuthenticated from "@/hooks/guards/useAuthenticated";
 
-// karwi: better name?
 export default function CollabRoom() {
-  // useAuthenticated();
   return <Room />;
 }
