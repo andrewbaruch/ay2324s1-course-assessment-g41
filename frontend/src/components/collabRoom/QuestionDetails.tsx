b1770b6225aef57bd68c87637c2d5c3321c44cd1
Kar Wi <t.karwi@yahoo.com>
2023-10-29 14:06:29 +0800

    feat: use chakra react select

--- frontend/src/components/collabRoom/QuestionDetails.tsx
+++ frontend/src/components/collabRoom/QuestionDetails.tsx
@@ -1,14 +1,23 @@
 // components/QuestionDetails.tsx
 import React from "react";
-import { Select, Tag, Text, Box } from "@chakra-ui/react";
+import { Tag, Text, Box } from "@chakra-ui/react";
 import { useCollabContext } from "src/hooks/contexts/useCollabContext";
+import { OptionBase, Select, SingleValue } from "chakra-react-select";
+
+interface OptionType extends OptionBase {
+  value: string;
+  label: string;
+}
 
 const QuestionDetails = () => {
   const { state, onQuestionChange, currentAttempt } = useCollabContext();
   const { questionTotalList } = state;
 
-  const handleQuestionChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    const newQuestionId = event.target.value;
+  const handleQuestionChange = (selectedOption: SingleValue<OptionType>) => {
+    if (!selectedOption) {
+      return;
+    }
+    const newQuestionId = selectedOption.value;
     if (currentAttempt) {
       onQuestionChange(newQuestionId, currentAttempt.attemptId);
     }
@@ -18,15 +27,19 @@ const QuestionDetails = () => {
     (question) => question.id === currentAttempt?.question.id,
   );
 
+  const options = questionTotalList.map((question) => ({
+    value: question.id,
+    label: question.title,
+  }));
+
   return currentAttempt ? (
     <Box>
-      <Select placeholder="Select a question" onChange={handleQuestionChange}>
-        {questionTotalList.map((question) => (
-          <option key={question.id} value={question.id}>
-            {question.title}
-          </option>
-        ))}
-      </Select>
+      <Select
+        options={options}
+        onChange={handleQuestionChange}
+        value={currentQuestion ? { value: currentQuestion.id, label: currentQuestion.title } : null}
+        placeholder="Select a question"
+      />
       {currentQuestion && (
         <>
           <Text>Difficulty: {currentQuestion.complexity}</Text>
