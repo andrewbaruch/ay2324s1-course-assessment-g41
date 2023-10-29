9b94dcb05554d32c35a0f656e5ee2320803aa942
Kar Wi <t.karwi@yahoo.com>
2023-10-29 16:03:22 +0800

    style: use chakra react select for lang

--- frontend/src/components/collabRoom/TopBar.tsx
+++ frontend/src/components/collabRoom/TopBar.tsx
@@ -1,22 +1,45 @@
 // components/TopBar.tsx
 import React from "react";
-import { Flex, Select } from "@chakra-ui/react";
+import { Flex } from "@chakra-ui/react";
 import { useCollabContext } from "src/hooks/contexts/useCollabContext";
+import { OptionBase, Select, SingleValue } from "chakra-react-select";
+
+interface OptionType extends OptionBase {
+  value: string;
+  label: string;
+}
 
 const TopBar = () => {
-  const { state, currentAttempt } = useCollabContext();
+  const { state, currentAttempt, onLanguageChange } = useCollabContext();
   const { languageTotalList } = state;
+
+  const handleLanguageChange = (selectedOption: SingleValue<OptionType>) => {
+    if (!selectedOption) {
+      return;
+    }
+    const newLanguageSlug = selectedOption.value;
+    if (currentAttempt) {
+      onLanguageChange(newLanguageSlug, currentAttempt.attemptId);
+    }
+  };
+
   const currentLanguage = currentAttempt?.language;
+  const options = languageTotalList.map((language) => ({
+    value: language.slug,
+    label: language.name,
+  }));
 
   return (
     <Flex align="center" justify="flex-start" pb={4}>
-      <Select value={currentLanguage?.slug} placeholder="Select language" maxWidth="200px">
-        {languageTotalList.map((language) => (
-          <option key={language.id} value={language.slug}>
-            {language.name}
-          </option>
-        ))}
-      </Select>
+      <Select
+        options={options}
+        onChange={handleLanguageChange}
+        value={
+          currentLanguage ? { value: currentLanguage.slug, label: currentLanguage.name } : null
+        }
+        placeholder="Select language"
+        maxWidth="200px"
+      />
       {/* Other components or elements can go here, and they will be positioned to the right of the Select component */}
     </Flex>
   );
