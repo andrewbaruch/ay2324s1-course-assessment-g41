e0a2a58657a3cd2cdae30ae25f2b11fa20a3bad8
Didymus <didymusne@gmail.com>
2023-11-10 22:32:40 +0800

    feat: add constraint for room

--- postgres-collab/migrations/01-room-table.sql
+++ postgres-collab/migrations/01-room-table.sql
@@ -5,7 +5,7 @@ CREATE TABLE Room (
     id SERIAL PRIMARY KEY,
     name TEXT NOT NULL UNIQUE,
     is_open BOOLEAN NOT NULL DEFAULT false,
-    user_count INTEGER DEFAULT 0,
+    user_count INTEGER DEFAULT 0 CHECK (user_count >= 0 AND user_count <= 2),
     attempt_id TEXT,
     created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
